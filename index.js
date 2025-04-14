0d562fc4515326ed382481b6d056c5a12369380f
ssebayigga-sharif <sharifsseba@gmail.com>
2025-04-14 13:31:57 +0300

    mypyramid

---
+++ index.js
@@ -0,0 +1,69 @@
+function change() {
+
+    setTimeout(() => {
+
+        document.getElementById("me").style.backgroundColor = "lavender";
+      
+      }, 2000);
+
+      setTimeout(() =>{
+
+        document.getElementById("him").style.backgroundColor = "pink"
+      }, 4000);
+      setTimeout(() =>{
+
+        document.getElementById("she").style.backgroundColor = "yellow"
+      }, 6000);
+      setTimeout(() =>{
+
+        document.getElementById("let").style.backgroundColor = "green"
+      }, 8000);
+      setTimeout(() =>{
+
+        document.getElementById("go").style.backgroundColor = "thistle"
+      }, 10000);
+      
+      setTimeout(() =>{
+
+        document.getElementById("am").style.backgroundColor = "red"
+      }, 12000);
+      
+      
+      
+};
+
+
+
+function black() {
+
+
+setTimeout(() => {
+
+  document.getElementById("me").style.backgroundColor = "black";
+
+}, 12000);
+
+setTimeout(() =>{
+
+  document.getElementById("him").style.backgroundColor = "black"
+}, 10000);
+setTimeout(() =>{
+
+  document.getElementById("she").style.backgroundColor = "black"
+}, 8000);
+setTimeout(() =>{
+
+  document.getElementById("let").style.backgroundColor = "black"
+}, 6000);
+setTimeout(() =>{
+
+  document.getElementById("go").style.backgroundColor = "black"
+}, 4000);
+
+setTimeout(() =>{
+
+  document.getElementById("am").style.backgroundColor = "black"
+}, 2000);
+
+
+}
